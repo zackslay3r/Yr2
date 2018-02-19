be9c3146bccd9aaf0add78150b20767b6163fdf6
Unknown <zackary.direen@hotmail.com>
2018-02-19 23:40:47 +1100

    testingg

--- Testing/PhysicsScene/Spring.cpp
+++ Testing/PhysicsScene/Spring.cpp
@@ -4,7 +4,7 @@
 
 
 
-Spring::Spring(RigidBody * body1, RigidBody * body2, float restLength, float springCoefficient, float damping, glm::vec2 contact1, glm::vec2 contact2)
+Spring::Spring(RigidBody * body1, RigidBody * body2, float restLength, float springCoefficient, glm::vec4 LineColour, float damping, glm::vec2 contact1, glm::vec2 contact2)
 {
 	m_body1 = body1;
 	m_body2 = body2;
@@ -14,6 +14,7 @@ Spring::Spring(RigidBody * body1, RigidBody * body2, float restLength, float spr
 	m_contact1 = contact1;
 	m_contact2 = contact2;
 	m_shapeID = ShapeType::JOINT;
+	m_colour = LineColour;
 }
 
 Spring::~Spring()
@@ -42,7 +43,7 @@ void Spring::fixedUpdate(glm::vec2 gravity, float timeStep)
 
 void Spring::makeGizmo()
 {
-	aie::Gizmos::add2DLine(m_body1->getPosition() + m_contact1, m_body2->getPosition() + m_contact2, glm::vec4(1, 1, 1, 1));
+	aie::Gizmos::add2DLine(m_body1->getPosition() + m_contact1, m_body2->getPosition() + m_contact2, m_colour);
 }
 
 void Spring::debug()
